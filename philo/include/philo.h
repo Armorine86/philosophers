54a47e19aa5766332d55941e4a10a954b2810900
mmondell <mmondell@c2r5p1.42quebec.com>
2021-09-02 15:49:37 -0400

    initial Push

---
+++ philo/include/philo.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mmondell <mmondell@student.42quebec.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/01 13:54:43 by mmondell          #+#    #+#             */
+/*   Updated: 2021/09/01 14:05:25 by mmondell         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+# include "libft.h"
+
+typedef struct s_philo
+{
+	int	eat;
+	int	sleep;
+	int	think;
+	
+}	t_philo;
+
+#endif
---
+++ philo/srcs/philosophers.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philosophers.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mmondell <mmondell@student.42quebec.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/01 13:54:41 by mmondell          #+#    #+#             */
+/*   Updated: 2021/09/01 14:06:21 by mmondell         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+int	main(int argc, char **argv)
+{
+	if (argc < 4)
+		ft_putstr_fd("Missing Arguments\n", 2);
+	argv++;
+	return (0);
+}
