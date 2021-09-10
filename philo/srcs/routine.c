80c46f372be612cd163ce5b840043ec1d021b83b
mmondell <mmondell@c2r5p1.42quebec.com>
2021-09-10 09:25:24 -0400

    ...

--- philo/srcs/routine.c
+++ philo/srcs/routine.c
@@ -5,19 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: mmondell <mmondell@student.42quebec.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/09/08 08:22:49 by mmondell          #+#    #+#             */
-/*   Updated: 2021/09/10 09:20:50 by mmondell         ###   ########.fr       */
+/*   Created: 2021/09/10 09:23:24 by mmondell          #+#    #+#             */
+/*   Updated: 2021/09/10 09:23:58 by mmondell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "philo.h"
-
-void	*meal_routine(void	*philo)
-{
-	t_info	*info;
-	int		i;
-
-	i = 0;
-	info = (t_info *)philo;
-	return (info);
-}
