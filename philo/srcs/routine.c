08ba3b2029b38ffc22cffcde0ec88699ed3d0439
mmondell <mmondell@c2r5p1.42quebec.com>
2021-09-13 09:26:01 -0400

    figuring out threads logic

--- philo/srcs/routine.c
+++ philo/srcs/routine.c
@@ -6,7 +6,7 @@
 /*   By: mmondell <mmondell@student.42quebec.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/10 09:23:24 by mmondell          #+#    #+#             */
-/*   Updated: 2021/09/10 09:43:35 by mmondell         ###   ########.fr       */
+/*   Updated: 2021/09/13 09:25:10 by mmondell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,10 @@ void	*meal_routine(void *philo)
 	
 	info = (t_info *)philo;
 	pthread_mutex_lock(info->print);
-	printf("total philos: %d\n", info->settings->total_philo);
+	printf("Philo id: %d,  total philos: %d\n",info->id, info->settings->total_philo);
+	printf("Philo id: %d,  time to die: %d\n",info->id, info->settings->time_die);
+	printf("Philo id: %d,  time to eat: %d\n",info->id, info->settings->time_eat);
+	printf("Philo id: %d,  time to sleep: %d\n",info->id, info->settings->time_sleep);
 	pthread_mutex_unlock(info->print);
 	return(info);
 }
