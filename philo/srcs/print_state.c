9345dc0ca94e94d5aafd0cc40acd2525d61fa7ce
mmondell <mmondell@c2r5p1.42quebec.com>
2021-10-19 09:21:22 -0400

    norm check

--- philo/srcs/print_state.c
+++ philo/srcs/print_state.c
@@ -6,7 +6,7 @@
 /*   By: mmondell <mmondell@student.42quebec.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/10 14:52:00 by mmondell          #+#    #+#             */
-/*   Updated: 2021/09/28 11:15:14 by mmondell         ###   ########.fr       */
+/*   Updated: 2021/10/19 09:20:59 by mmondell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,6 @@ void	print_state(t_philo *p, char *str)
 	if (p->state == s_eat)
 		p->last_meal = time_now();
 	printf("\e[36m%-3ld\e[0m || \e[32mphilo \e[33m%d \e[32m%s\e[0m\n",
-		   current_time, p->id, str);
+		current_time, p->id, str);
 	pthread_mutex_unlock(&p->m->print_lock);
 }
